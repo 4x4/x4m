bd666f9a6715c01b92d1e6033c059bd92ccfa90c
Bobrya <abiatop@gmail.com>
2013-01-09 01:16:30 +0300

    sortable more porblems jquery ui

--- _adm/xjs/_components/slotz/slotz.js
+++ _adm/xjs/_components/slotz/slotz.js
@@ -12,9 +12,25 @@ var Slotz = new Class({
         
         initiateSlotz:function()
         {
-            $('.def-slot ul').sortable({
-                connectWith: '.def-slot ul'
-            });
+            $('#modulec_footer,#modulec_right_side').sortable({
+               placeholder: "placeholder",
+                tolerance: "intersect",
+             axis: "y",
+            forceHelperSize: true,
+                cursorAt:{top:1,left:1},
+                revert:true,
+                 
+                       connectWith: ".connectedSortable"
+              
+            }).disableSelection();
+                           
+                           
+                           
+            //$( "#modulec_footer,#modulec_right_side" ).sortable( "option", "connectWith", ".connectedSortable" );
+            
+                              
+            
+            
             
         }  
         
